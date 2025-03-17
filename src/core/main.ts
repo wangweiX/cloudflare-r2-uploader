37295ccfcab9d17e852bb96ae9e90fbb15daadbf
w7years <w7years@gmail.com>
2025-03-17 09:13:25 +0800

    🎨 style(main.ts): update upload icon size for better visibility in the UI

--- src/core/main.ts
+++ src/core/main.ts
@@ -9,7 +9,7 @@ import {Logger} from '../utils/logger';
 import {StorageProvider} from '../models/storage-provider';
 
 // 上传当前文件按钮的图标
-const UPLOAD_ICON = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="17 8 12 3 7 8"></polyline><line x1="12" y1="3" x2="12" y2="15"></line></svg>`;
+const UPLOAD_ICON = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="17 8 12 3 7 8"></polyline><line x1="12" y1="3" x2="12" y2="15"></line></svg>`;
 
 /**
  * 插件主类
