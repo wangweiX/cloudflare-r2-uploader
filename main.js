0be37c265b8c2e076738cfb1c371aebd65ddb6b4
w7years <w7years@gmail.com>
2025-03-17 09:28:58 +0800

    🎨 style(main.js, main.ts): Update upload icon SVG for improved display and consistency in size

--- main.js
+++ main.js
@@ -1023,7 +1023,12 @@ var SettingsTab = class extends import_obsidian6.PluginSettingTab {
 };
 
 // src/core/main.ts
-var UPLOAD_ICON = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="17 8 12 3 7 8"></polyline><line x1="12" y1="3" x2="12" y2="15"></line></svg>`;
+var UPLOAD_ICON = `
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="100" height="100" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+<path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+<polyline points="17 8 12 3 7 8"></polyline>
+<line x1="12" y1="3" x2="12" y2="15"></line>
+</svg>`;
 var CloudflareImagesUploader = class extends import_obsidian7.Plugin {
   constructor() {
     super(...arguments);
